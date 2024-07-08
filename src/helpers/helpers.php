bd790f528a92af542a300bcf4c1ebd91a8c6014d
ErjonStafa <erjonstafa647@gmail.com>
2024-07-08 11:08:41 +0200

    Better os detection

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -8,8 +8,19 @@ if (! function_exists('erjon_decrypt')) {
 }
 
 if(! function_exists('get_os')){
+    /**
+     * Return linux or windows
+     *
+     * @return string
+     */
     function get_os()
     {
-        return php_uname();
+        $os = php_uname();
+
+        if (preg_match('/Linux/', $os)){
+            return 'Linux';
+        }
+
+        return 'Windows';
     }
 }
